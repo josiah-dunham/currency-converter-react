76a99411605fa605fa31b8a0e73685ba4c0c5b60
Josiah Dunham <dunham.josiahp@gmail.com>
2018-08-30 11:12:57 -0400

    initial page setup with DOM elements rendered via React

--- src/index.js
+++ src/index.js
@@ -2,20 +2,78 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function AmountInput(props) {
+    return (
+        <input type="text" defaultValue={props.amount} className={props.inputClassName} onChange={props.onChange}/>
+    );
+}
+
+function CodeInput(props) {
+    return (
+		<select defaultValue={props.codeValue} onChange={props.onChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+		</select>
+    );
+}
 
 class Converter extends React.Component {
 	constructor(props) {
         super(props);
         this.state = {
-            title: 'My React Currency Converter'
+            baseAmount: "1.00",
+			baseCode: "USD",
+			targetAmount: "8.77",
+			targetCode: "NOK"
         };
     }
 	
+	handleAmountChange() {
+		console.log(this.state.baseAmount);
+	}
+	
+	handleCodeChange() {
+		console.log(this.state.baseCode);
+	}
+	
+	renderAmount(isBase) {
+		const amount = (isBase) ? this.state.baseAmount : this.state.targetAmount;
+		const className = (isBase) ? "base-amount" : "target-amount";
+		return (
+			<AmountInput
+				amount={amount}
+				inputClassName={className}
+				onChange={() => this.handleAmountChange()}
+			/>
+		);
+	}
+	
+	renderCode(isBase) {
+		const code = (isBase) ? this.state.baseCode : this.state.targetCode;
+		const className = (isBase) ? "base-code" : "target-code";
+		return (
+			<CodeInput
+				codeValue={code}
+				selectClassName={className}
+				onChange={() => this.handleCodeChange()}
+			/>
+		);
+	}
+	
     render() {
         return (
-          <div>
-            {this.state.title}  
-          </div>
+			<div>
+				<div className="base-inputs">
+					{this.renderAmount(true)}
+					{this.renderCode(true)}	
+				</div>
+				<div className="target-inputs">
+					{this.renderAmount(false)}
+					{this.renderCode(false)}
+				</div>
+			</div>
         );
     }  
 }
